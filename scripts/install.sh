e7661cd1fbf48fdc4d9ab48f8c401941b93f7e32
b3hr4d <b3hr4d@live.com>
2023-06-25 12:40:52 +0300

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,10 +4,7 @@ fi
 
 dfx nns install
 
-# get --enable-bitcoin from args
-if [[ $* == *--enable-bitcoin* ]]; then
-    sh scripts/ckbtc.sh
-    sh scripts/kyt.sh
-    sh scripts/minter.sh
-    sh scripts/index.sh
-fi
+sh scripts/ckbtc.sh
+sh scripts/kyt.sh
+sh scripts/minter.sh
+sh scripts/index.sh
