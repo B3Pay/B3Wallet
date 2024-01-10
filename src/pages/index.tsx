dd1c3b445fa66befb5e179dfb557bd84da24151c
b3hr4d <behradmusiclove@yahoo.com>
2024-01-10 16:48:29 +0300

    Remove FormEditor component from HomePage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,6 @@ import Login from "@src/components/Login"
 import { Box } from "@src/components/ui/box"
 import { useSystemAuthClient } from "@src/service/system"
 import Account from "@src/components/Account"
-import FormEditor from "@src/components/FormEditor"
 
 function HomePage() {
   const { identity } = useSystemAuthClient()
@@ -11,9 +10,9 @@ function HomePage() {
 
   return (
     <Box className="flex flex-col space-y-2">
-      {/* {!isAnonymous && <Account />}
-      <Login /> */}
-      <FormEditor />
+      {!isAnonymous && <Account />}
+      <Login />
+      {/* <FormEditor /> */}
     </Box>
   )
 }
